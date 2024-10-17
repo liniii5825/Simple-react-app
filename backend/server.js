6e8f4bc808b18aa93f67b1dbd0dc7eb73681fbf2
Lin_2z <liniii@foxmail.com>
2024-10-18 00:28:32 +0800

    make frontend part static

--- backend/server.js
+++ backend/server.js
@@ -2,17 +2,26 @@ import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
 import productRoutes from "./routes/product.route.js";
+import path from "path";
 
 dotenv.config();
-
-// console.log(process.env.MONGO_URI);
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolve();
+
 app.use(express.json()); // allow us to accept json data in the req.body
 
 app.use("/api/products", productRoutes); // use the productRoutes for any routes that start with /api/products
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist"))); // serve static files from the dist folder
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
+  ); // serve the index.html file for all other routes
+}
 
 app.listen(PORT, () => {
   connectDB();
